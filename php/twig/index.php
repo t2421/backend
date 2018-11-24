a0b35ce7e583c9273343098ff21956ad4c8a4c3f
Taiga Kiyotaki <t2421gm@gmail.com>
2018-11-24 17:53:07 +0900

    php twigサンプル追加

---
+++ php/twig/index.php
@@ -0,0 +1,26 @@
+<?php
+require_once __DIR__.'/bootstrap.php';
+// Create a product list
+$products = [
+    [
+        'name'          => 'Notebook',
+        'description'   => 'Core i7',
+        'value'         =>  800.00,
+        'date_register' => '2017-06-22',
+    ],
+    [
+        'name'          => 'Mouse',
+        'description'   => 'Razer',
+        'value'         =>  125.00,
+        'date_register' => '2017-10-25',
+    ],
+    [
+        'name'          => 'Keyboard',
+        'description'   => 'Mechanical Keyboard',
+        'value'         =>  250.00,
+        'date_register' => '2017-06-23',
+    ],
+];
+
+// Render our view
+echo $twig->render('index.html', ['products' => $products] );
